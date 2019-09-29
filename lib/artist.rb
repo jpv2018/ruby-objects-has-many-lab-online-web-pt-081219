d2f0f14d22f10db41056828a61be702ea409ba9a
Jason Viola <jason.viola@mavs.uta.edu>
2019-09-29 21:06:30 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,8 @@
 class Artist
   attr_accessor :name
   
+  @@count = 0
+  
   def initialize(name)
     @name = name
     @songs = []
@@ -13,6 +15,7 @@ class Artist
   def add_song(song)
     song.artist = self
     self.songs << song
+    @@count += 1
   end
   
   def add_song_by_name(song_name)
@@ -21,7 +24,7 @@ class Artist
   end
   
   def self.song_count
-    Song.count
+    @@count
   end
   
 end
