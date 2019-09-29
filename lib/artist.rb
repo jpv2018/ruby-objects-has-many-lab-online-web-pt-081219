10a3f709b8081041f1ac46a603d4d4b2ecbee728
Jason Viola <jason.viola@mavs.uta.edu>
2019-09-29 20:50:22 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,9 +6,12 @@ class Artist
     @songs = []
   end
   
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+    
   def add_song(song)
     song.artist = self
-    @songs << song
   end
   
   def add_song_by_name(song_name)
